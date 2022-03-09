645b65937bf76bf9850bcd0a3c99caab1f1d0ee1
Yik Hong Chan <harryyikhchan@gmail.com>
2022-03-08 17:09:41 -0800

    Import tidyverse, here and plotly

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'tidyverse', 'here', 'plotly'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
